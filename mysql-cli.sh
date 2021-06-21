f8cdc3656bf7612abca3f27fe6988834dc8063ef
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-06-21 12:21:54 +0300

    Used newest mysql-cli.sh

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,7 +1,6 @@
 #! /bin/bash -e
 
-docker run $* \
-   --name mysqlterm --rm \
-   -e MYSQL_PORT_3306_TCP_ADDR=$DOCKER_HOST_IP \
-   mysql:5.7.13  \
-   sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P3306 -uroot -prootpassword -o eventuate'
+docker run $*  \
+   --network eventuate-tram-core-reactive-examples-basic_default \
+   --name mysqlterm  --rm mysql:5.7.13 \
+   sh -c 'exec mysql -hmysql -P3306 -uroot -prootpassword eventuate'
